67c5a54431ea6537e06db22d8f2828e28ed0d559
Stephen Haffner <kd8zev@users.noreply.github.com>
2014-11-07 17:16:44 -0500

    Fixed several fatal bugs

--- vim/install.sh
+++ vim/install.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
+cd ../
 DIR=$(pwd)
-CTAGS_VERSION = "5.8"
-CTAGS_URL = "http://prdownloads.sourceforge.net/ctags/ctags-$CTAGS_VERSION.tar.gz"
+CTAGS_VERSION=5.8
+CTAGS_URL=http://prdownloads.sourceforge.net/ctags/ctags-$CTAGS_VERSION.tar.gz
 
 echo "Cloning submodules"
 git submodule update --init --recursive 
-
 cd ./vim
+
 echo "Backing up old vim congif"
-mv ~/.vimrc ~/.vimrc.bak
+mv -f ~/.vimrc ~/.vimrc.bak
 mv ~/.vim ~/.vim.bak
 
 echo "Copying new vim config"
-cp ./vim/vimrc ~/.vimrc
-cp -rf ./vim/vim ~/.vim
+cp ./vimrc ~/.vimrc
+cp -rf ./vim ~/.vim
 
 # Delete the below section iff you already have Exuberant cTags intalled
 echo "Preparing to build Exuberant cTags"
@@ -23,11 +24,11 @@ cd vim_build
 echo "Downloading source"
 wget $CTAGS_URL
 echo "Extracting source"
-tar -xf ctags-$CTAGS_VERSION.tar.zg
+tar -xf ctags-$CTAGS_VERSION.tar.gz
 cd ctags-$CTAGS_VERSION
 echo "Building ctags from source"
 ./configure
-./make
+make
 echo "Copying binary"
 mkdir ~/.vim/bin
 cp ./ctags ~/.vim/bin/ctags
